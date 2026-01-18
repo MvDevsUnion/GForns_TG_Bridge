a54de68f76295033b2078042546a5b76becdc3b0
fISHIE <83373559+WhoIsFishie@users.noreply.github.com>
2026-01-18 12:24:20 +0500

    added better formatting

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 function onFormSubmit(e) {
-  const sheet = e.range.getSheet();
-  const row = e.range.getRow();
-  const lastCol = sheet.getLastColumn();
+const sheet = e.range.getSheet();
+const row = e.range.getRow();
 
-  const headers = sheet.getRange(1, 2, 1, lastCol - 1).getValues()[0];
-  const values = sheet.getRange(row, 2, 1, lastCol - 1).getValues()[0];
+const rawDate = sheet.getRange(row, 1).getValue();
+const evidence = sheet.getRange(row, 2).getValue();
 
-  let message = 'New form submission:\n\n';
-  headers.forEach((h, i) => {
-    message += `${h}: ${values[i]}\n`;
-  });
+const formattedDate = Utilities.formatDate(
+new Date(rawDate),
+Session.getScriptTimeZone(),
+'dd-MM-yyyy'
+);
 
-  sendTelegramMessage(message);
+const message =
+`## Company Name
+
+Industry:
+
+Issues:
+
+Issue 1
+
+Issue 2
+
+Evidence: ${evidence}
+
+Date Added: ${formattedDate}`;
+
+sendTelegramMessage(message);
 }
 
 
-
 const TELEGRAM_TOKEN = ':';
-const CHAT_ID = '';
+const CHAT_ID = '-';
 
 function sendTelegramMessage(text) {
   const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
 
   const payload = {
     chat_id: CHAT_ID,
-    text: text,
+    text: text
   };
 
   const options = {
